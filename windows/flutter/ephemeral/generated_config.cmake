2d28d916272e25627551b2810aa2471671a4cb0f
Hilmi Arya Rafwa Muhammad <165791286+hilmiarm@users.noreply.github.com>
2024-06-09 15:33:21 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\HP\\Documents\\Pemrograman Mobile\\UTS\\feb_upn" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\HP\\Documents\\Pemrograman Mobile\\UTS\\feb_upn"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\HP\\Documents\\Pemrograman Mobile\\UTS\\feb_upn\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\HP\\Documents\\Pemrograman Mobile\\UTS\\feb_upn"
+  "FLUTTER_TARGET=C:\\Users\\HP\\Documents\\Pemrograman Mobile\\UTS\\feb_upn\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNDBlOTc2YmVkZmY1N2U2OWUxYjNkODlhN2MyYTNjNjE3YTAzZGFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\HP\\Documents\\Pemrograman Mobile\\UTS\\feb_upn\\.dart_tool\\package_config.json"
+)
